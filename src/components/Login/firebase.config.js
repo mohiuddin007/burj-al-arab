63f1b134d9c9a156ec42987135ca33c7b480ed42
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-09-28 23:37:24 +0600

    frist commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNODZYOrN0GSPCyVvGOIvHtD5nxU8lCPE",
+    authDomain: "burj-al-arab-f7bcb.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-f7bcb.firebaseio.com",
+    projectId: "burj-al-arab-f7bcb",
+    storageBucket: "burj-al-arab-f7bcb.appspot.com",
+    messagingSenderId: "686139286576",
+    appId: "1:686139286576:web:a0dbc9b007282e8761f9f5"
+  };
+
+  export default firebaseConfig;
